5c61ea7f054aa3247556507eaddf68a98a2b8b62
Kaz <kaz@askblockchain.co.uk>
2024-03-23 19:13:39 +0000

    deleted random notes

--- routes/index.js
+++ routes/index.js
@@ -83,14 +83,3 @@ module.exports = router;
 
 
 
-
-
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// module.exports = router;
